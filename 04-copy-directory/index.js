293c28cd05322167b0896e5ad8502a65d761b15b
Андрей Нестерук <anjeinesty@gmail.com>
2021-11-07 20:59:28 +0300

    feat: task number 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fsPromises = require('fs/promises');
+const path = require("path");
 
+const filesDirectory = path.join(__dirname, 'files');
+const copyDirectory = path.join(__dirname, 'files-copy');
+
+const getFilesToCopy = async () => {
+  await clearCopy(copyDirectory);
+  const arrayFiles = await fsPromises.readdir(filesDirectory, { encoding: 'utf-8', withFileTypes: true });
+  await fsPromises.mkdir(copyDirectory);
+  await Promise.all(arrayFiles.map((el) => {
+    return copyFile(el.name);
+  }));
+}
+
+const copyFile = (nameFile) => {
+  const fileFrom = path.join(filesDirectory, nameFile);
+  const fileTo = path.join(copyDirectory, nameFile);
+  fsPromises.copyFile(fileFrom, fileTo)
+}
+
+const clearCopy = async (distPath) => {
+  await fsPromises.rm(distPath, { force: true, recursive: true });
+};
+
+getFilesToCopy();
